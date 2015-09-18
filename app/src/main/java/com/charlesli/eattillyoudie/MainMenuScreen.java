4a42601bee3d030442d0644c1c1cf2aba85ec96a
--add <jordan_li80@hotmail.com>
2015-09-18 14:20:29 -0400

    Added Main Menu Screen

--- app/src/main/java/com/charlesli/eattillyoudie/MainMenuScreen.java
+++ app/src/main/java/com/charlesli/eattillyoudie/MainMenuScreen.java
@@ -1,7 +1,39 @@
 package com.charlesli.eattillyoudie;
 
+import com.charlesli.androidgames.framework.Game;
+import com.charlesli.androidgames.framework.Screen;
+
 /**
  * Created by Li on 2015/9/18.
  */
-public class MainMenuScreen {
+public class MainMenuScreen extends Screen {
+
+    public MainMenuScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void present(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
